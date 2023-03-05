86bde7b4e1854c6e8bacf0de13ba722a4a6c3745
septemblue <focusramxram@gmail.com>
2023-03-05 16:26:49 +0700

    Make the list interactive

--- src/main/kotlin/VideoList.kt
+++ src/main/kotlin/VideoList.kt
@@ -1,3 +1,4 @@
+import kotlinx.browser.window
 import react.FC
 import react.Props
 import react.dom.html.ReactHTML.p
@@ -17,10 +18,18 @@ val VideoList = FC<VideoListProps>() { props ->
     for (video in props.videos) {
         p {
             key = video.id.toString()
+            onClick = {
+                window.alert("Clicker $video")
+            }
             +"${video.speaker}:  ${video.title}"
         }
     }
 }
 /*The key attribute helps the React renderer figure out what to do when the value of props.videos changes.
 It uses the key to determine which parts of a list need to be refreshed and which ones stay the same
+ */
+/*
+Defining an onClick function directly as lambda is concise and very useful for prototyping. However, due to the way
+equality currently works in Kotlin/JS, performance-wise it's not the most optimized way to pass click handlers.
+If you want to optimize rendering performance, consider storing your functions in a variable and passing them
  */
