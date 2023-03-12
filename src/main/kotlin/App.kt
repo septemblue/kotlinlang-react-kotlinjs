ff3b1ef09a14c87fadc463a8208c87735567cd7f
Raffael Ali Maulana <raffael.tuasamu@iseller.id>
2023-03-12 21:16:07 +0700

    Move video lists to the application state

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -16,14 +16,6 @@ data class Video(
     val speaker: String,
     val videoURL: String
 )
-val unwatchedVideos = listOf(
-    Video(1, "Opening Keynote", "Andrey Breslav", "https://youtu.be/PsaFVLr8t4E"),
-    Video(2, "Dissecting the stdlib", "Huyen Tue Dao", "https://youtu.be/Fzt_9I733Yg"),
-    Video(3, "Kotlin and Spring Boot", "Nicolas Frankel", "https://youtu.be/pSiZVAeReeg")
-)
-val watchedVideos = listOf(
-    Video(4, "Creating Internal DSLs in Kotlin", "Venkat Subramaniam", "https://youtu.be/JzTeAM8N1-o")
-)
 /*
 The FC function creates a function component.
 The Kotlin wrappers for React come with a domain-specific language (DSL) that makes it possible to write HTML in pure
@@ -31,6 +23,15 @@ The Kotlin wrappers for React come with a domain-specific language (DSL) that ma
   benefits of a statically typed language, such as autocomplete or type checking.
  */
 val App = FC<Props> {
+    var unwatchedVideos: List<Video> by useState(listOf(
+        Video(1, "Opening Keynote", "Andrey Breslav", "https://youtu.be/PsaFVLr8t4E"),
+        Video(2, "Dissecting the stdlib", "Huyen Tue Dao", "https://youtu.be/Fzt_9I733Yg"),
+        Video(3, "Kotlin and Spring Boot", "Nicolas Frankel", "https://youtu.be/pSiZVAeReeg")
+    ))
+    var watchedVideos: List<Video> by useState(listOf(
+        Video(4, "Creating Internal DSLs in Kotlin", "Venkat Subramaniam", "https://youtu.be/JzTeAM8N1-o")
+    ))
+
     var currentVideo : Video? by useState(null)
     h1 {
         +"KotlinConf Explorer"
@@ -60,6 +61,16 @@ val App = FC<Props> {
     currentVideo?.let {curr ->
         VideoPlayer {
             video = curr
+            unwatchedVideo = curr in unwatchedVideos
+            onWatchedVideoPressed = {
+                if (video in unwatchedVideos) {
+                    unwatchedVideos = unwatchedVideos - video
+                    watchedVideos = watchedVideos + video
+                } else {
+                    watchedVideos = watchedVideos - video
+                    unwatchedVideos = unwatchedVideos + video
+                }
+            }
         }
     }
 }
