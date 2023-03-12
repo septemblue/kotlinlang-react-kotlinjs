fe285e0efd73519c4aa97d03dbc737edf9559727
Raffael Ali Maulana <raffael.tuasamu@iseller.id>
2023-03-12 21:09:46 +0700

    add a button and wire it

--- src/main/kotlin/VideoPlayer.kt
+++ src/main/kotlin/VideoPlayer.kt
@@ -1,9 +1,12 @@
+import csstype.Display
+import csstype.NamedColor
 import csstype.Position
 import csstype.px
 import emotion.react.css
 import react.FC
 import react.Props
 import react.dom.html.ReactHTML
+import react.dom.html.ReactHTML.button
 import react.dom.html.ReactHTML.div
 import react.dom.html.ReactHTML.h3
 import react.dom.html.ReactHTML.img
@@ -13,6 +16,8 @@ To tell Kotlin that a certain declaration is written in pure JavaScript, you sho
  */
 external interface VideoPlayerProps : Props {
     var video: Video
+    var onWatchedVideoPressed: (Video) -> Unit
+    var unwatchedVideo: Boolean
 }
 
 val VideoPlayer = FC<VideoPlayerProps> {props ->
@@ -24,6 +29,20 @@ val VideoPlayer = FC<VideoPlayerProps> {props ->
         }
         h3 {
             +"${props.video.speaker}: ${props.video.title}"
+        }
+        button {
+            css {
+                display = Display.block
+                backgroundColor = if (props.unwatchedVideo) NamedColor.lightgreen else NamedColor.red
+            }
+            onClick = {
+                props.onWatchedVideoPressed(props.video)
+            }
+            if (props.unwatchedVideo) {
+                + "Mark as watched"
+            } else {
+                + "Mark as unwatched"
+            }
         }
         img {
             src = "https://via.placeholder.com/640x360.png?text=Video+Player+Placeholder"
